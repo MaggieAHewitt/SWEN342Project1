8dab63feab63fbcd21362141efacf061244941a0
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2014-10-19 21:14:52 -0400

    Added shell class for lead developer and statuses enum class

---
+++ src/LeadDeveloper.java
@@ -0,0 +1,16 @@
+
+public class LeadDeveloper extends Developer{
+
+	public LeadDeveloper(int t, int e){
+		super(t, e);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		(new LeadDeveloper(1,1)).start();
+	}
+
+}
---
+++ src/Status.java
@@ -0,0 +1,8 @@
+
+public enum Status {
+	MEETING,
+	ASKING,
+	LUNCHING,
+	WORKING,
+	WAITING
+}
