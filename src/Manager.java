78159fb76895e0e312b99bd9b5f9e0c4a93d6994
bbensch <bbensch@live.com>
2014-10-24 10:33:19 -0400

    Updated File, timings set
    Might run. Not sure. Didn't test it. Still figuring out how to communicate
    from Dev/employee to Manager.

--- src/Manager.java
+++ src/Manager.java
@@ -2,20 +2,97 @@ public class Manager extends Thread{
 	
 	public Manager() {
 		private static ARRIVAL_TIME = 0;
-		private static DEPARTURE_TIME = 8*60*10;
+		private static DEPARTURE_TIME = 9*60*10;
+		Status status;
 	}
 
 	public void run() {
+		int startLunchtime = 4*60*10;
+		int endLunchtime = 5*60*10;
+		int questionTime = 10*10;
+		int morningMeetingStart = 2*60*10;
+		int morningMeetingEnd = 3*60*10;
+		int afternoonMeetingStart = 6*60*10;
+		int afternoonMeetingEnd = 7*60*10;
+		int groupMeetingStart = 8*60*10;
+
 		printArrivalMessage(ARRIVAL_TIME);
 		
-		//Other day things!
+		//Work until meeting happens
+
+		while (System.currentTimeMillis() <= morningMeetingStart + ARRIVAL_TIME) {
+			//check for question to be answered
+			//work for 10ms
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		//At 11AM Meeting
+		while (System.currentTimeMillis() <= morningMeetingEnd + ARRIVAL_TIME) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedExcetion e) {
+				e.printStackTrace();
+			}
+		}
+
+		while (System.currentTimeMillis() <= Lunch + ARRIVAL_TIME) {
+			//check for question to be answered
+			//else work for 10ms
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		printLunchMessage(System.currentTimeMillis());
+
+		while (System.currentTimeMillis() < endLunchtime + ARRIVAL_TIME) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		printEndLunchMessage(System.currentTimeMillis);
+
+		while (System.currentTimeMillis() <= afternoonMeetingStart + ARRIVAL_TIME) {
+			//Check for question to be answered
+			//work for 10ms
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		printAfternoonMeetingMessage(System.currentTimeMillis);
+
+		while (System.currentTimeMillis() <= groupMeetingStart + ARRIVAL_TIME) {
+			//check for question to be answered
+			//check for 4PM (8*60*10)
+			//else work for 10ms
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		while (System.currentTimeMillis() < DEPARTURE_TIME + ARRIVAL_TIME) {
+			//Check for question to be answered
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 		printDepartureMessage(DEPARTURE_TIME);
 	}
 	
-	private long Lunch() {
-		int lunchtime = 4*60*10;
-	}
 	
 	private long AnswerQuestion() {
 		int timeSpent = 10*10;
@@ -48,15 +125,31 @@ public class Manager extends Thread{
 	}
 	
 	private void printLunchMessage(long time) {
-		System.out.println(getTime(time) + "Manager goes to Lunch!");
+		System.out.println(getTime(time) + "  Manager goes to Lunch!");
 	}
 	
+	private void printMorningMeetingMessage (long time) {
+		System.out.println(getTime(time) + "  Manager goes to morning meeting!");
+	}
+
+	private void printAfternoonMeetingMessage (long time) {
+		System.out.println(getTime(time) + "  Manager goes to Afternoon Meeting!");
+	}
+
+	private void printStatusMeetingMessage(long time) {
+		System.out.println(getTime(time) + "  Manager goes to Status Meeting");
+	}
+
+	private void printBackToWorkMessage (long time) {
+		System.out.println(getTime(time) + "  Manager goes back to work!");
+	}
+
 	private void printAnswerQMessage(long time) {
-		System.out.println(getTime(time) + "Manager's answering a question!");
+		System.out.println(getTime(time) + "  Manager's answering a question!");
 	}
 	
 	private void printDepartureMessage(long time) {
-		System.out.println(getTime(time) + "Manager gracefully leaves for the day!");
+		System.out.println(getTime(time) + "  Manager gracefully leaves for the day!");
 	}
 
 } 
