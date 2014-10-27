05317bcc92b67c0ab08038713f8dae46fa6ab205
Brad Bensch <bbensch@live.com>
2014-10-26 23:40:13 -0400

    bugfix

--- src/main.java
+++ src/main.java
@@ -14,9 +14,9 @@ public class main {
 		
 		Manager item = new Manager(time, DEVS);
 		
-		Developer lead1 = new Developer(item,time,1,1);
-		Developer lead2 = new Developer(item,time,2,1);
-		Developer lead3 = new Developer(item,time,3,1);
+		Developer lead1 = new Developer(item,time,1,1,questionChance);
+		Developer lead2 = new Developer(item,time,2,1,questionChance);
+		Developer lead3 = new Developer(item,time,3,1,questionChance);
 		
 		item.start();
 		lead1.start();
@@ -24,15 +24,15 @@ public class main {
 		lead3.start();
 		
 		
-		(new Developer(item,lead1,time,1,2)).start();
-		(new Developer(item,lead1,time,1,3)).start();
-		(new Developer(item,lead1,time,1,4)).start();
-		(new Developer(item,lead1,time,2,2)).start();
-		(new Developer(item,lead1,time,2,3)).start();
-		(new Developer(item,lead1,time,2,4)).start();
-		(new Developer(item,lead1,time,3,2)).start();
-		(new Developer(item,lead1,time,3,3)).start();
-		(new Developer(item,lead1,time,3,4)).start();
+		(new Developer(item,lead1,time,1,2,questionChance)).start();
+		(new Developer(item,lead1,time,1,3,questionChance)).start();
+		(new Developer(item,lead1,time,1,4,questionChance)).start();
+		(new Developer(item,lead1,time,2,2,questionChance)).start();
+		(new Developer(item,lead1,time,2,3,questionChance)).start();
+		(new Developer(item,lead1,time,2,4,questionChance)).start();
+		(new Developer(item,lead1,time,3,2,questionChance)).start();
+		(new Developer(item,lead1,time,3,3,questionChance)).start();
+		(new Developer(item,lead1,time,3,4,questionChance)).start();
 	}
 
 }
